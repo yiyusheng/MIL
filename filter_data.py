f223408b358580343a88458a1b49a64b7fde987c
yiyusheng-crane23 <yiyusheng.hust@gmail.com>
2019-05-14 10:30:21 +0800

    update merge_data.py

--- filter_data.py
+++ filter_data.py
@@ -12,7 +12,8 @@ import pandas as pd
 import os
 
 home = os.getenv("HOME")
-data = pd.read_csv(home+'/Data/backblaze/data_bb',sep=',')
+path_data = '/Data/backblaze/'
+data = pd.read_csv(home+path_data+'data_2013',sep=',')
 
 data_names = data.columns.values
 data_names_meta = data_names[0:5]
@@ -22,3 +23,11 @@ sta_null = data[data_names_attr].isnull().sum()/len(data)*100
 valid_names = sta_null.index[sta_null<=10].values
 
 data_filter = data[data_names_meta.tolist()+valid_names.tolist()]
+data_filter.info()
+data_filter.describe()
+
+model_count = data_filter['model'].value_counts()/len(data_filter)
+valid_model = model_count[0:3]
+for m in valid_model.index.values:
+    data_model = data_filter[data_filter['model']==m]
+    data_model.to_csv(home+path_data+m,index=0)
